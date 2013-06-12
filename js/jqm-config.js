613b8fb4334b99fe3eba4cc822e7a1b98439d16c
Vadim Mirgorod <dealancer@gmail.com>
2013-06-12 17:41:34 +0300

    refactoring

--- js/jqm-config.js
+++ js/jqm-config.js
@@ -1,20 +1,20 @@
 $(document).bind("mobileinit", function () {
+
+  // Disable jQueryMobile routing.
   $.mobile.ajaxEnabled = false;
   $.mobile.linkBindingEnabled = false;
   $.mobile.hashListeningEnabled = false;
   $.mobile.pushStateEnabled = false;
 
-  // var transitions = ((navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/iPod/i))) ? true : false;
-  transitions = true;
-
+  // Setup transitions and effects.
   $.extend($.mobile, {
-    slideText :  (transitions) ? "slide" : "none",
-    slideUpText :  (transitions) ? "slideup" : "none",
-    defaultPageTransition:(transitions) ? "slide" : "none",
-    defaultDialogTransition:(transitions) ? "slideup" : "none"
+    slideText: "slide",
+    slideUpText: "slideup",
+    defaultPageTransition: "slideup",
+    defaultDialogTransition: "slideup"
   });
 
-  // Remove page from DOM when it's being replaced
+  // Remove page from the DOM when it's being replaced
   $('div[data-role="page"]').live('pagehide', function (event, ui) {
       $(event.currentTarget).remove();
   });
