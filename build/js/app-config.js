8b5586af7483fa80210c99532bccecfec001aee2
Vadim Mirgorod <dealancer@gmail.com>
2013-06-18 22:43:14 +0300

    updted build

--- build/js/app-config.js
+++ build/js/app-config.js
@@ -1,1 +1,1 @@
-define(["jquery","backbone","backbone-mongodb"],function(e,t){return e.support.cors=!0,e.ajaxSetup({cache:!1}),_.extend(t.Model.prototype,t.MongoModel.mixin),{baseURL:"https://api.mongolab.com/api/1/databases/social-mobile-app/collections/",addURL:"?apiKey=yGobEjzhT76Pjo9RaOLGfA89xCJXegpl"}});
+define(["jquery","backbone"],function(e,t){return e.support.cors=!0,e.ajaxSetup({cache:!1}),_.extend(t.Model.prototype,t.MongoModel.mixin),{baseURL:"https://api.mongolab.com/api/1/databases/social-mobile-app/collections/",addURL:"?apiKey=yGobEjzhT76Pjo9RaOLGfA89xCJXegpl"}});
